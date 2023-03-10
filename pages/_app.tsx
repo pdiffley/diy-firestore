68cb2b91ffe8a5e9c739d6d293d86a5ff67c6a81
Shane Moore <shane@shanemoore.me>
2023-03-10 01:14:27 +0000

    Add some styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import "@code-hike/mdx/dist/index.css";
+import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function DiyFirestoreApp({ Component, pageProps }: AppProps) {
